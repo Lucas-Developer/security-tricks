bc8400afa43c823dc2f1f801d1b5bde29a8468e7
Guilherme Asm <keerok@protonmail.com>
2017-07-15 20:00:42 -0300

    Create index.php

---
+++ webVulnApp/index.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+  </head>
+  <body>
+    <a href="./xss/">XSS</a>
+    <br>
+    <a href="./fileUpload/">File Upload</a>
+    <br>
+    <a href="./crsf/">CSRF</a></h3>
+    <br>
+    <a href="./sqli/">SQLI</a>
+    <br>
+    <a href="./xxe/">XXE</a>
+    <br>
+    <a href="./lfi_rfi/">LFI RFI</a>
+    <br>
+    <a href="./ssrf/">SSRF</a>
+    <br>
+    <a href="./crlf/">CRLF</a>
+    <br>
+    <a href="./ssti/">SSTI</a>
+    <br>
+    <a href="./idor/">IDOR</a>
+    <br>
+    <a href="./object/">Object Deserialization</a>
+    <br>
+    <a href="./phishing/">Phishing</a>
+    <br>
+    <a href="./expression/">Expression Language Injection</a>
+  </body>
+</html>
