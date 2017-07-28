0749497210cf7c03aa87d9bd2caf4d780e85fcc0
Guilherme Asm <keerok@protonmail.com>
2017-07-27 21:17:23 -0300

    Create change_cenarius.php

---
+++ webVulnApp/crsf/change_cenarius.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CRSF Demo</title>
+		<h1>CRSF</h1>
+	</head>
+	<body>
+
+		<form method="post" action="change_data.php">
+			<input name="username" type="text">
+			<br>
+			<input name="password" type="password">
+			<br>
+			<input name="confirmPassword" type="password">
+			<br>
+			<input type="submit" value="Submit">
+		</form>
+	</form>
+</html>
